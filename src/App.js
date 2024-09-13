e13e9aaf5f6f1ae9a5ef0d6f7f7e92d495eb65ad
wildroses12 <ana.mladenovic2001@gmail.com>
2024-09-13 17:05:53 +0200

    kreirana komponenta koja predstavlja kontakt formu aplikacije

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
+// App.js
+
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import HomePage from './HomePage';
+import ContactForm from './ContactForm';
 
 function App() {
+  const [messages, setMessages] = useState(() => {
+    // Inicijalizacija poruka iz lokalne memorije
+    const savedMessages = localStorage.getItem('messages');
+    return savedMessages ? JSON.parse(savedMessages) : [];
+  });
+
+  useEffect(() => {
+    // Spremanje poruka u lokalnu memoriju kad se niz poruka promijeni
+    localStorage.setItem('messages', JSON.stringify(messages));
+  }, [messages]);
+
+  const addMessage = (newMessage) => {
+    setMessages((prevMessages) => [...prevMessages, newMessage]);
+  };
+
   return (
     <div className="App">
-       <HomePage></HomePage>
+      <HomePage />
+      <ContactForm addMessage={addMessage} />
     </div>
   );
 }
---
+++ src/ContactForm.js
@@ -0,0 +1,53 @@
+ 
+
+import React, { useState } from 'react';
+import './ContactForm.css';
+
+function ContactForm({ addMessage }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newMessage = { name, email, message };
+    addMessage(newMessage);
+    // Resetiranje forme
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
+  return (
+    <div className="contact-form-section">
+      <div className="form-container">
+        <h2>Kontaktirajte nas</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Vaše ime"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <input
+            type="email"
+            placeholder="Vaša email adresa"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <textarea
+            placeholder="Vaša poruka"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          ></textarea>
+          <button type="submit">Pošalji</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ContactForm;
