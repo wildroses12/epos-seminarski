623a5bf36aa78d2613641ae3504887f3e825e348
teodoram3 <teodoramilutinovic3@gmail.com>
2024-09-13 18:03:01 +0200

    pretraga i sortiranje

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,14 +1,51 @@
- 
-
-import React from 'react';
+import React, { useState } from 'react';
 import './ProductList.css';
 
 function ProductList({ products }) {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortOption, setSortOption] = useState('');
+
+  // Filtriranje proizvoda na osnovu unetog termina pretrage
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    product.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  // Sortiranje proizvoda na osnovu izabrane opcije
+  const sortedProducts = [...filteredProducts].sort((a, b) => {
+    if (sortOption === 'price-asc') {
+      return parseFloat(a.price) - parseFloat(b.price);
+    } else if (sortOption === 'price-desc') {
+      return parseFloat(b.price) - parseFloat(a.price);
+    } else {
+      return 0;
+    }
+  });
+
   return (
     <div className="product-list">
       <h2>Naši Proizvodi</h2>
+
+      <div className="controls">
+        <input
+          type="text"
+          placeholder="Pretraži proizvode..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+
+        <select
+          value={sortOption}
+          onChange={(e) => setSortOption(e.target.value)}
+        >
+          <option value="">Sortiraj</option>
+          <option value="price-asc">Cena: od najniže</option>
+          <option value="price-desc">Cena: od najviše</option>
+        </select>
+      </div>
+
       <div className="product-grid">
-        {products.map((product) => (
+        {sortedProducts.map((product) => (
           <div className="product-card" key={product.id}>
             <img src={product.image} alt={product.name} />
             <h3>{product.name}</h3>
